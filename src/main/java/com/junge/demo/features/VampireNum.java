4381e9132ae53f2d9e5c942b196f532a1f726d79
junge8618 <513851613@qq.com>
2018-08-17 11:37:41 +0800

    更新吸血鬼数字算法

--- src/main/java/com/junge/demo/features/VampireNum.java
+++ src/main/java/com/junge/demo/features/VampireNum.java
@@ -3,6 +3,10 @@
  */
 package com.junge.demo.features;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 吸血鬼数字
  * 吸血鬼数字是指位数为偶数的数字，可由一对数字相乘而得到，这对数字各包含乘积的一半位数的数字，以两个0结尾的数字是不允许的，第一个数字顺序可以随意，第二个数字顺序不能变。
@@ -15,37 +19,34 @@ public class VampireNum {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(vampirenum(4));
+		vampirenum();
 
 	}
 
 	/**
-	 * 
-	 * @param digitcpa 位数
-	 * @return
+	 * 逆向思维，4位数字的吸血鬼数字只能拆分成两个2位数，因此遍历所有两个两位数相乘的情况，除去不符合的情况不用判断，其他挨个判断即可
 	 */
-	public static String vampirenum(int digitcpa) {
-		StringBuilder result = new StringBuilder();
+	public static void vampirenum() {
 		
-		int min = (int)Math.pow(10, digitcpa - 1);
-		int max = (int)Math.pow(10, digitcpa);
-		System.out.println(min + "," + max);
-		
-		for (int num=min; num<max; num++) {
-			if (isVampireNum(digitcpa, num)) {
-				result.append(num).append(",");
-			}
-		}
-		
-		return result.toString();
-	}
-	
-	private static boolean isVampireNum(int digitcpa, int num) {
-		for (int i=0; i<digitcpa; i++) {
-			for(int j=i; j<digitcpa; j++) {
+		for(int i=10; i<=99; i++) {
+			for (int j= i + 1; j<=99; j++) {
+				int i_target = i * j;
+				if (i_target < 1000 || i_target > 9999) {
+					continue;
+				}
+				
+				char[] target_chars = String.valueOf(i_target).toCharArray();
+				char[] tmp_chars = String.valueOf(i + "" + j).toCharArray();
+				Arrays.sort(target_chars);
+				Arrays.sort(tmp_chars);
+				if (Arrays.equals(target_chars, tmp_chars)) {
+					System.out.println(i_target + "=" + i + "*" + j);
+				}
 				
 			}
 		}
-		return false;
+		
 	}
+	
+	
 }
